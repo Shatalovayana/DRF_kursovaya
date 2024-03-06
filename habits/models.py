9c0f3607ff20c0c1e847c4b7fafd3d90b7453475
Yana <yanashat198@gmail.com>
2024-03-06 20:46:05 +0700

    startapp habits

---
+++ habits/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Habit(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    place = models.CharField(max_length=100, verbose_name='место', **NULLABLE)
+    time = models.DateTimeField(verbose_name='время')
+    action = models.CharField(max_length=200, verbose_name='действие')
+    sign_of_a_pleasant_habit = models.BooleanField(verbose_name='признак приятной привычки', **NULLABLE)
+    connected_habit = models.CharField(max_length=150, verbose_name='связанная привычка', **NULLABLE)
+    period = models.CharField(max_length=50, verbose_name='периодичность')
+    reward = models.CharField(max_length=150, verbose_name='вознаграждение', **NULLABLE)
+    time_to_complete = models.PositiveIntegerField(verbose_name='время на выполнение')
+    is_published = models.BooleanField(verbose_name='признак публичности', default=False)
