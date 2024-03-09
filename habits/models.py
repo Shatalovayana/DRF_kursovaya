886871afe39a619c1375b9053904bbcb37e2d9da
Yana <yanashat198@gmail.com>
2024-03-09 22:31:45 +0700

    create models Habit and User

--- habits/models.py
+++ habits/models.py
@@ -5,13 +5,14 @@ NULLABLE = {'null': True, 'blank': True}
 
 
 class Habit(models.Model):
+    """Модель привычки"""
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     place = models.CharField(max_length=100, verbose_name='место', **NULLABLE)
     time = models.DateTimeField(verbose_name='время')
     action = models.CharField(max_length=200, verbose_name='действие')
     sign_of_a_pleasant_habit = models.BooleanField(verbose_name='признак приятной привычки', **NULLABLE)
-    connected_habit = models.CharField(max_length=150, verbose_name='связанная привычка', **NULLABLE)
-    period = models.CharField(max_length=50, verbose_name='периодичность')
+    connected_habit = models.ForeignKey('self', verbose_name='связанная привычка', on_delete=models.CASCADE, **NULLABLE)
+    period = models.IntegerField(verbose_name='периодичность')
     reward = models.CharField(max_length=150, verbose_name='вознаграждение', **NULLABLE)
     time_to_complete = models.PositiveIntegerField(verbose_name='время на выполнение')
     is_published = models.BooleanField(verbose_name='признак публичности', default=False)
