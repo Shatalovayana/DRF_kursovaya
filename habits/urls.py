1bd9f0b473dd3e95a47f6d8157da5e8adb4ab0a4
Yana <yanashat198@gmail.com>
2024-03-09 22:32:40 +0700

    add views and urls for habits and users

---
+++ habits/urls.py
@@ -0,0 +1,15 @@
+from habits.apps import HabitsConfig
+from django.urls import path
+
+from habits.views import HabitCreateAPIView, HabitListAPIView, HabitRetrieveAPIView, HabitUpdateAPIView, \
+    HabitDestroyAPIView
+
+app_name = HabitsConfig.name
+
+urlpatterns = [
+    path('habits/create/', HabitCreateAPIView.as_view(), name='habit-create'),
+    path('habits/', HabitListAPIView.as_view(), name='habit-list'),
+    path('habits/<int:pk>/', HabitRetrieveAPIView.as_view(), name='habits-get'),
+    path('habits/update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habits-update'),
+    path('habits/delete/<int:pk>/', HabitDestroyAPIView.as_view(), name='habits-delete'),
+]
