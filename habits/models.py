9c6e07e32e9c64243ae4960efbb0405a4e1f0561
Yana <yanashat198@gmail.com>
2024-03-12 13:45:16 +0700

    change habits

--- habits/models.py
+++ habits/models.py
@@ -8,11 +8,11 @@ class Habit(models.Model):
     """Модель привычки"""
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     place = models.CharField(max_length=100, verbose_name='место', **NULLABLE)
-    time = models.DateTimeField(verbose_name='время')
+    time = models.TimeField(verbose_name='время', **NULLABLE)
     action = models.CharField(max_length=200, verbose_name='действие')
     sign_of_a_pleasant_habit = models.BooleanField(verbose_name='признак приятной привычки', **NULLABLE)
     connected_habit = models.ForeignKey('self', verbose_name='связанная привычка', on_delete=models.CASCADE, **NULLABLE)
     period = models.IntegerField(verbose_name='периодичность')
     reward = models.CharField(max_length=150, verbose_name='вознаграждение', **NULLABLE)
-    time_to_complete = models.PositiveIntegerField(verbose_name='время на выполнение')
+    time_to_complete = models.PositiveIntegerField(verbose_name='время на выполнение', default=1)
     is_published = models.BooleanField(verbose_name='признак публичности', default=False)
